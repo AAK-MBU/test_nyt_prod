0db901b1a8de3a800e30557aa6237fa3e35c9f25
MBURKU <135531844+MBURKU@users.noreply.github.com>
2025-08-22 10:17:27 +0200

    Refactor main.py to improve readability and structure

--- main.py
+++ main.py
@@ -17,49 +17,26 @@
 # subprocess.run(command_args, check=True)
 
 
-# """The main file of the robot which will install all requirements in
-# a virtual environment and then start the actual process.
-# """
-
-# import subprocess
-# import os
-# import sys
-
-# script_directory = os.path.dirname(os.path.realpath(__file__))
-# os.chdir(script_directory)
-
-# # Install uv
-# subprocess.run([sys.executable, "-m", "pip", "install", "uv"], check=True)
-
-# # Create virtual environment
-# subprocess.run(["uv", "venv"], check=True)
-
-# # Install packages in the virtual environment
-# subprocess.run(["uv", "pip", "install", "."], check=True)
-
-# command_args = [r".venv\Scripts\python", "-m", "robot_framework"] + sys.argv[1:]
-
-# subprocess.run(command_args, check=True)
+"""The main file of the robot which will install all requirements in
+a virtual environment and then start the actual process.
+"""
 
 import subprocess
 import os
 import sys
-from pathlib import Path
 
-script_dir = Path(__file__).resolve().parent
-os.chdir(script_dir)
+script_directory = os.path.dirname(os.path.realpath(__file__))
+os.chdir(script_directory)
 
-def run_py_uv(*args):
-    subprocess.run([sys.executable, "-m", "uv", *args], check=True)
-
+# Install uv
 subprocess.run([sys.executable, "-m", "pip", "install", "uv"], check=True)
 
-run_py_uv("venv")
+# Create virtual environment
+subprocess.run(["uv", "venv"], check=True)
 
-run_py_uv("pip", "install", ".")
+# Install packages in the virtual environment
+subprocess.run(["uv", "pip", "install", "."], check=True)
 
-venv = script_dir / ".venv"
-py = venv / "Scripts" / "python.exe"
-cmd = [str(py), "-m", "robot_framework"] + sys.argv[1:]
+command_args = [r".venv\Scripts\python", "-m", "robot_framework"] + sys.argv[1:]
 
-subprocess.run(cmd, check=True)
+subprocess.run(command_args, check=True)
