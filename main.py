519988bd2d0c3b1936a78d248de5bc5a99ff9c15
MBURKU <135531844+MBURKU@users.noreply.github.com>
2025-08-22 09:36:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,14 +29,14 @@ script_directory = os.path.dirname(os.path.realpath(__file__))
 os.chdir(script_directory)
 
 # Install uv
-subprocess.run([sys.executable, "-m", "pip", "install", "uv"], check=True)
+subprocess.run([sys.executable, "-m", "pip", "install", "uv"], check=True, shell=True)
 
 # Create virtual environment
-subprocess.run(["uv", "venv"], check=True)
+subprocess.run(["uv", "venv"], check=True, shell=True)
 
 # Install packages in the virtual environment
-subprocess.run(["uv", "pip", "install", "."], check=True)
+subprocess.run(["uv", "pip", "install", "."], check=True, shell=True)
 
 command_args = [r".venv\Scripts\python", "-m", "robot_framework"] + sys.argv[1:]
 
-subprocess.run(command_args, check=True)
+subprocess.run(command_args, check=True, shell=True)
